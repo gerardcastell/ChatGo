63e0662763aafb3627ce9fff982f8e43d4cc8ea5
Gerard <gerardcastell97@gmail.com>
2018-12-16 18:36:43 +0100

    Solved problems with disconnection and end of file

--- client/client.go
+++ client/client.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net"
 	"os"
+ 	"bufio"
+ 	"fmt"
+	"net"
 	"sync"
 )
 
 var wg sync.WaitGroup
-
 // Tell the 'wg' WaitGroup how many threads/goroutines
 //   that are about to run concurrently.
 
-func main() {
+func main(){
 	wg.Add(2)
 	nick := newUser()
 
@@ -28,19 +27,20 @@ func main() {
 	wg.Wait()
 }
 
-func newUser() (nick string) {
+func newUser ()(nick string) {
 	fmt.Println("Introduce tu nick:")
 	reader := bufio.NewReader(os.Stdin)
-	nick, err_nick := reader.ReadString('\n')
-	checkError(err_nick, "")
+	input, err_input := reader.ReadString('\n')
+	checkError(err_input, "")
+	nick = input[0:len(input)-1]
 	return nick
 }
 
-func sender(connection net.Conn, nick string) {
+func sender(connection net.Conn, nick string){
 	reader := bufio.NewReader(os.Stdin)
 	message, _, _ := reader.ReadLine()
 
-	for message != nil {
+	for message != nil{
 		_, err := connection.Write(message)
 		checkErrorAndCloseConn(err, "", connection)
 		if err != nil {
@@ -51,11 +51,10 @@ func sender(connection net.Conn, nick string) {
 		}
 		message, _, _ = reader.ReadLine()
 	}
-
 	wg.Done()
 }
 
-func receiver(connection net.Conn) {
+func receiver(connection net.Conn){
 	buf := make([]byte, 1000)
 	input, err := connection.Read(buf)
 	checkError(err, "")
@@ -69,23 +68,24 @@ func receiver(connection net.Conn) {
 			fmt.Println("Connection closed...")
 			return
 		}
-		fmt.Println(string(message))
+		fmt.Print(string(message))
 		input, err = connection.Read(buf)
 		message = buf[:input]
 		checkErrorAndCloseConn(err, "", connection)
-	}
+	} 
 	wg.Done()
 }
 
-func checkError(err error, txt string) {
+func checkError(err error, txt string){
 	message := "Fatal error: "
-	if txt == "" {
+	if txt == ""{
 		message = txt
 	}
 	if err != nil {
-		fmt.Println(message, err.Error())
+			fmt.Println(message, err.Error())
 	}
 }
+
 
 func checkErrorAndCloseConn(err error, txt string, conn net.Conn) {
 	message := "Fatal error: "
--- server/server.go
+++ server/server.go
@@ -1,35 +1,33 @@
 package main
-
 import "fmt"
 import "net"
 
-//import "os"
-//import "bufio"
-
 var (
-	clients = make(map[string]net.Conn)
+        clients = make(map[string]net.Conn)
 )
 
-func main() {
-	fmt.Println("Launching server...")
-	// listen on all interfaces
-	server, err := net.Listen("tcp", "localhost:1234")
-	if err == nil {
-		fmt.Println("Server listening on port 1234")
-	} else {
-		checkError(err)
-	}
+func main(){
+        fmt.Println()
+        fmt.Println("Launching server...")
+        // listen on all interfaces
+        server, err := net.Listen("tcp", "localhost:1234")
+        if err == nil {
+                fmt.Println("Server listening on port 1234")
+                fmt.Println()
+        } else {
+                checkError(err)
+        }
 
-	defer server.Close()
+        defer server.Close()
 
-	for {
-		// accept connection on port
-		conn, err := server.Accept()
-		checkError(err)
-		if conn != nil {
-			go openConnection(conn)
-		}
-	}
+        for {
+                // accept connection on port
+                conn, err := server.Accept()
+                checkError(err)
+                if (conn != nil) {
+                        go openConnection(conn)
+                }
+        }
 }
 
 func isNickInUse(nick string) (isUsed bool) {
@@ -43,88 +41,86 @@ func isNickInUse(nick string) (isUsed bool) {
 
 func openConnection(connection net.Conn) {
 
-	nickBuffer := make([]byte, 256)
-	nickRawDataSize, err := connection.Read(nickBuffer)
-	checkErrorAndCloseConn(err, connection)
-	if err != nil {
+        nickBuffer := make([]byte, 256)
+        nickRawDataSize, err := connection.Read(nickBuffer)
+        checkErrorAndCloseConn(err, connection)
+        if err != nil {
 		return
 	}
-	nickRawData := nickBuffer[:nickRawDataSize]
-	nick := string(nickRawData)
+        nickRawData := nickBuffer[:nickRawDataSize]
+        nick := string(nickRawData)
 
-	a := ""
+        isNickUsed := isNickInUse(nick)
+        nickUsed := "Nickname in use. Please enter a new nick: "
 
-	isNickUsed := isNickInUse(nick)
-
-	for isNickUsed {
-		a = "Nickname in use. Please enter a new nick: "
-		connection.Write([]byte(a))
+        for isNickUsed {
+		connection.Write([]byte(nickUsed))
 		nickRawDataSize, _ := connection.Read(nickBuffer)
 		nickRawData := nickBuffer[:nickRawDataSize]
 		nick = string(nickRawData)
 		isNickUsed = isNickInUse(nick)
 	}
+  
 
-	for foreignNick := range clients {
-		if foreignNick == nick {
-			a = "Nickname in use. Please enter a new nick: "
-			connection.Write([]byte(a))
-			nickRawDataSize, _ := connection.Read(nickBuffer)
-			nickRawData := nickBuffer[:nickRawDataSize]
-			nick = string(nickRawData)
-			break
-		}
-	}
+        for foreignNick := range clients {
+                if foreignNick == nick {
+                        connection.Write([]byte(nickUsed))
+                        nickRawDataSize, _ := connection.Read(nickBuffer)
+                        nickRawData := nickBuffer[:nickRawDataSize]
+                        nick = string(nickRawData)
+                        break
+                }
+        }
+        newUserTxt := fmt.Sprintln("New user: ", nick)
+        fmt.Print(newUserTxt)
+        infoTxt := fmt.Sprintln(nick, "has joined the room")
+        for _, clientConn := range clients {
+                infoTxt = fmt.Sprintln(nick, "has joined the room")
+                clientConn.Write([]byte(infoTxt))
+        }
 
-	fmt.Println("New user: " + nick)
-	info := ""
-	for _, clientConn := range clients {
-		info = nick + "has joined the room"
-		clientConn.Write([]byte(info))
-	}
+        clients[nick] = connection
+        messageBuffer := make([]byte, 1000)
+        disconnectCommand := "exit"
 
-	clients[nick] = connection
-	messageBuffer := make([]byte, 1400)
-	disconnectCommand := "exit"
-
-	for {
-		messageRawDataSize, err := connection.Read(messageBuffer)
-		checkErrorAndCloseConn(err, connection)
+        for {
+                messageRawDataSize, err := connection.Read(messageBuffer)
+                checkErrorAndCloseConn(err, connection)
 		if err != nil {
 			delete(clients, nick)
-			fmt.Println(nick + "disconnected")
+			fmt.Println(nick + " disconnected")
 			return
 		}
-		messageRawData := messageBuffer[:messageRawDataSize]
-		message := string(messageRawData)
-
-		if message == disconnectCommand {
-			dis_msg := nick + "disconnected"
-			fmt.Println(dis_msg)
-			delete(clients, nick)
-			connection.Close()
-			for _, client := range clients {
-				_, err := client.Write([]byte(dis_msg))
-				checkErrorAndCloseConn(err, client)
-			}
-			return
-		} else {
-			response := nick + ": " + message
-			for username, client := range clients {
-				if username != nick {
-					_, err := client.Write([]byte(response))
-					checkErrorAndCloseConn(err, client)
-
-				}
-			}
-		}
-	}
+                messageRawData := messageBuffer[:messageRawDataSize]
+                message := string(messageRawData)
+                
+                dis_msg := fmt.Sprintln(nick, " disconnected")
+                if message == disconnectCommand {
+                        dis_msg = fmt.Sprintln(nick, " disconnected")
+                        fmt.Println(dis_msg)
+                        delete(clients, nick)
+                        connection.Close()
+                        for _, client := range clients {
+                               _, err := client.Write([]byte(dis_msg))
+                               checkErrorAndCloseConn(err, client)
+                        }
+                        return
+                } else {
+                        response := fmt.Sprintln(nick, ": ", message)
+                        for username, client := range clients {
+                                if username != nick {
+                                        _, err := client.Write([]byte(response))
+                                        checkErrorAndCloseConn(err, client)
+                                }
+                         }      
+                }
+        }
 }
 
 func checkError(err error) {
-	if err != nil {
-		fmt.Println("Fatal error ", err.Error())
-	}
+        if err != nil {
+                fmt.Println("Fatal error ", err.Error())
+        }
 }
 
 func checkErrorAndCloseConn(err error, conn net.Conn) {
